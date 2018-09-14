ee4eca518d56e01667cdae746ada4c520ec085b8
B533P-21\533 <miam@hotmail.com>
2018-09-14 15:24:44 -0400

    such miam

---
+++ Cours4/Assets/Script/DialogDisplayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogDisplayer : MonoBehaviour {
+    private Text textComponent;
+
+	// Use this for initialization
+	void Awake () {
+        textComponent = gameObject.GetComponentInChildren<Text>();
+	}
+	
+    public void SetDialogText(string newDialogText)
+    {
+        textComponent.text = newDialogText;
+    }
+
+    public void CloseDialog()
+    {
+        Destroy(gameObject);
+    }
+	
+}
---
+++ Cours4/Assets/Script/DialogManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+
+    [SerializeField] public GameObject dialogPrefab;
+    [SerializeField] public GameObject mainCanvas;
+
+    private bool actionAxisInUse = true;
+    private GameObject player;
+    private bool dialogInitiated = false;
+    private DialogText currentDialog;
+    private DialogDisplayer currentdialogDisplayer;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    private void Update()
+    {
+        ProcessInput();
+
+    }
+
+    public void StartDialog(DialogText newDialog)
+    {
+        dialogInitiated = true;
+        player.GetComponent<PlayerMovement>().DisableControl();
+        currentDialog = newDialog;
+        GameObject currentDialogObject = Instantiate(dialogPrefab, mainCanvas.transform);
+        currentdialogDisplayer = currentDialogObject.GetComponent<DialogDisplayer>();
+        currentdialogDisplayer.SetDialogText(currentDialog.GetDialogText());
+    }
+
+    public void ProcessInput()
+    {
+        if (ShouldProccessInput())
+        {
+            actionAxisInUse = true;
+            if(currentDialog.IsNextDialog())
+            {
+                currentDialog = currentDialog.GetNextDialog();
+                currentdialogDisplayer.SetDialogText(currentDialog.GetDialogText());
+            }
+            else
+            {
+                EndDialog();
+            }
+            
+        }
+        ValideAxisInUse();
+    }
+
+    public void EndDialog()
+    {
+        dialogInitiated = false;
+        currentdialogDisplayer.CloseDialog();
+        player.GetComponent<PlayerMovement>().EnableControl();
+        currentDialog = null;
+
+    }
+
+    private bool ShouldProccessInput()
+    {
+        if(dialogInitiated)
+        {
+            if (!actionAxisInUse &&  Input.GetAxis("Jump") != 0)
+            {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private void ValideAxisInUse()
+    {
+        if (Input.GetAxis("Jump") != 0)
+        {
+            actionAxisInUse = true;
+        }
+        else
+        {
+            actionAxisInUse = false;
+        }
+    }
+}
